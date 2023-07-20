319a9d6fb73328ea50975362cffaed76aad75081
Keenan Nicholson <Keenanbnicholson@gmail.com>
2023-07-20 19:56:44 -0230

    baby's first tailwind

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,7 @@
-import "./NavBar.css";
-
 export const NavBar = () => {
   return (
-    <div className="heading">
-      <h1>Hiking Mode</h1>
+    <div className="bg-white text-black text-center m-0 p-0">
+      <h1 className="m-0 p-5">Hiking Mode</h1>
     </div>
   );
 };
